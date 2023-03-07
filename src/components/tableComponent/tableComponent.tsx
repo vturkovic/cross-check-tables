6726fd7d420d17e1ae7a229fb468abd49edf2407
vturkovic <vladimir.turkovic0@gmail.com>
2023-03-07 14:43:19 +0100

    added table horizontal scroll

--- src/components/tableComponent/tableComponent.tsx
+++ src/components/tableComponent/tableComponent.tsx
@@ -11,36 +11,38 @@ const TableComponent = ({ columns, data }: any) => {
     } = useTable({ columns, data });
 
     return (
-        <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()}>
-                                {column.render('Header')}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, index) => {
-                    prepareRow(row);
-                    const rowClassName = index % 2 === 0 ? 'even' : 'odd';
-                    return (
-                        <tr {...row.getRowProps()} className={rowClassName}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td {...cell.getCellProps()} >
-                                        {cell.value !== null ? cell.render('Cell') : '-'}
-                                    </td>
-                                );
-                            })}
+        <div className="table-container" style={{ overflowX: 'auto' }}>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render('Header')}
+                                </th>
+                            ))}
                         </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, index) => {
+                        prepareRow(row);
+                        const rowClassName = index % 2 === 0 ? 'even' : 'odd';
+                        return (
+                            <tr {...row.getRowProps()} className={rowClassName}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()} >
+                                            {cell.value !== null ? cell.render('Cell') : '-'}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
